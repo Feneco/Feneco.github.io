3ae395d90ef0d55ac7fe7dd6476d35f3e3461898
Wagner <wagnerweb2010@gmail.com>
2023-11-02 23:16:21 -0300

    began work on calculator

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    export let text: string;
+    export let funUp: string | null = null;
+    export let funDown: string | null = null;
+    export let style: string | null = null;
+</script>
+
+<div class="wrapper" style={style??""}>
+    <button class="calcButton" id="calcButton" on:click>
+        <div class="buttonText">{text}</div>
+        <div class="smallButton down">{funDown ?? ' '}</div>
+    </button>
+    <label for="calcButton" class="smallButton up">{funUp ?? ' '}</label>
+</div>
+
+<style lang="scss">
+    @use '../globals/colors.scss';
+    @import '@fontsource-variable/inter';
+
+    .wrapper {
+        display: flex;
+        height: 70px;
+        min-width: 67px;
+        flex-direction: column-reverse;
+        align-items: center;
+        font-family: 'Inter Variable';
+    }
+
+    .calcButton {
+        padding-top: 6px;
+        display: flex;
+        height: 50px;
+        max-height: 50px;
+        flex-direction: column;
+        align-self: stretch;
+        gap: 5px;
+
+        border: none;
+        border-radius: 5px;
+        background: #4a4a4a;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    }
+
+    .calcButton:hover{
+        background: #636363;
+    }
+
+    .calcButton:active{
+        background: #8b8b8b;
+        box-shadow: inset 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    }
+
+    .buttonText {
+        color: white;
+        font-size: 20px;
+        text-transform: uppercase;
+    }
+
+    .smallButton {
+        text-align: center;
+        font-size: 10px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        text-transform: lowercase;
+    }
+
+    .up {
+        color: colors.$upColor;
+    }
+
+    .down {
+        color: colors.$downColor;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,64 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    import Button from "$lib/components/Button.svelte";
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+    <div class="innerButtons">
+        <Button text={"DUP"} />
+        <Button text={"ROT"} />
+        <Button text={"SWAP"} />
+        <Button text={"DROP"} />
+        <Button text={"RCL"} />
 
-		to your new<br />SvelteKit app
-	</h1>
+        <Button text={"/"} on:click={(e) => console.log(e.target?.innerText)}/>
+        <Button text={"7"} on:click={(e) => console.log(e.target?.innerText)}/>
+        <Button text={"8"} />
+        <Button text={"9"} />
+        <Button text={"SUM"} />
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+        <Button text={"X"} />
+        <Button text={"4"} />
+        <Button text={"5"} />
+        <Button text={"6"} />
+        <Button text={"+/-"} />
+        
+        <Button text={"-"} />
+        <Button text={"1"} />
+        <Button text={"2"} />
+        <Button text={"3"} />
+        <Button text={"1/x"} />
 
-	<Counter />
+        <Button text={"+"} />
+        <Button text={"0"} />
+        <Button text={"."} />
+        <Button text={"enter"} style={"grid-column: 4/6;"}/>
+    </div>
 </section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+<style lang="scss">
+    section {
+        background-color: white;
+        display: flex;
+        width: 400px;
+        padding: 20px;
+        flex-direction: column;
+        align-items: center;
+        gap: 10px;
+        flex-shrink: 0;
 
-	h1 {
-		width: 100%;
-	}
+        background: #E9E9E9;
+        box-shadow: 0px 25px 12px -12px rgba(0, 0, 0, 0.25); 
+    }
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    .innerButtons {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+        gap: 10px;
+        overflow: hidden;
+    }
 </style>
